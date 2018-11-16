454c102a9e9d2f6e5305da1f5d8f476ef20ec11f
maestro\Artem <artem.lucky@gmail.com>
2018-11-16 21:42:12 +0200

    added user with role 'Manager'

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'support@maestrotools.ru',
+            'password' => bcrypt('secret'),
+            'role' => 'manager',
+        ]);
     }
 }
